1c9e1f18b6b87d8e9c00f812aa9acb56e161a53e
Ahmed <ahmedchitos@gmail.com>
2025-01-17 19:52:48 +0200

    Add project files

--- DistributedLoggingSystem/obj/Release/net8.0/DistributedLoggingSystem.AssemblyInfo.cs
+++ DistributedLoggingSystem/obj/Release/net8.0/DistributedLoggingSystem.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("DistributedLoggingSystem")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ffa1ff33386f8dc46e7a524d2fca34bf1558b36d")]
 [assembly: System.Reflection.AssemblyProductAttribute("DistributedLoggingSystem")]
